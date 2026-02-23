c6b6921611cbdf669d07d132fa5b263272604fb2
Rachana078 <rachanareetu2000@gmail.com>
2026-02-22 18:42:56 -0800

    Authentication System Structure

---
+++ src/main/java/in/rachana/authify/io/ProfileRequest.java
@@ -0,0 +1,13 @@
+package in.rachana.authify.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProfileRequest {
+
+    private String name;
+    private String email;
+    private String password;
+}
